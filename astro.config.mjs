ee034a968ec8f0e8a731ac460dcc58c7960648c9
Croustys <akos.barabas2002@gmail.com>
2022-09-10 18:27:52 +0200

    SEO, cv dload, error fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-	integrations: [preact()],
+	site: 'https://barabasakos.hu',
+	integrations: [preact(), robotsTxt()],
 });

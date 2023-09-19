ee4dac65efb3ecc6d4b49724fb8d64930f3e69d1
Ákos Barabás <akos.barabas2002@gmail.com>
2023-09-19 11:36:54 +0200

    data update & version bump

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
-import Styles from './styles.module.scss';
+import Styles from "./styles.module.scss";
 
 function Footer() {
-	return (
-		<footer className={Styles.footer}>
-			&copy; {new Date().getFullYear()} Barabás Ákos
-			<small className={Styles.byline}>Built by Barabás Ákos</small>
-		</footer>
-	);
+  return (
+    <footer className={Styles.footer}>
+      &copy; {new Date().getFullYear()} Barabás Ákos
+      <small className={Styles.byline}>2132 Göd, Ménesi utca 16</small>
+      <small className={Styles.byline}>58469598</small>
+      <small className={Styles.byline}>48185686-1-33</small>
+      <small className={Styles.byline}>Built by Barabás Ákos</small>
+    </footer>
+  );
 }
 export default Footer;

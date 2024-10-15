7238db496519ea336750dc2b2c3e95176bf93d39
croustys <akos.barabas2002@gmail.com>
2024-10-15 14:26:45 +0200

    design changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,10 +4,6 @@ function Footer() {
   return (
     <footer className={Styles.footer}>
       &copy; {new Date().getFullYear()} Barabás Ákos
-      <small className={Styles.byline}>2132 Göd, Ménesi utca 16</small>
-      <small className={Styles.byline}>58469598</small>
-      <small className={Styles.byline}>48185686-1-33</small>
-      <small className={Styles.byline}>Built by Barabás Ákos</small>
     </footer>
   );
 }
